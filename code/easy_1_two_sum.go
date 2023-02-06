d68f5d5cff863cf1fbe0f58401ba7b38baeceffd
rym49 <hatanakaryoma.130@gmail.com>
2023-02-06 23:00:40 +0900

    1-改良
    hashmapを使い、O(n^2)からO(n)に改良
    https://www.code-recipe.com/post/two-sum#viewer-9p12l

--- code/easy_1_two_sum.go
+++ code/easy_1_two_sum.go
@@ -10,11 +10,18 @@ package code
 //
 //lint:ignore U1000 //
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if target == nums[i]+nums[j] {
-				return []int{i, j}
-			}
+	// hashmapを使う方法
+	// https://www.code-recipe.com/post/two-sum#viewer-9p12l
+	// 最初にnumsを全てhashmapにしてしまうと、同値のものの扱いに困る
+	// numsを走査しつつ、(target - 走査対象の一要素)の値がhashmapにあれば、その要素のインデックスとhashmapの値が答えになる
+	// 無ければhashmapにその要素をキー、要素位置をバリューとし追加する
+	hashmap := make(map[int]int, 0)
+
+	for i, v := range nums {
+		if j, ok := hashmap[target-v]; ok {
+			return []int{j, i}
+		} else {
+			hashmap[v] = i
 		}
 	}
 
