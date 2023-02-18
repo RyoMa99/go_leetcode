ba24ab6fb238ae416276032bedaabff45e927585
rym49 <hatanakaryoma.130@gmail.com>
2023-02-19 02:54:58 +0900

    26-模範解答
    戻り値でないnumsを書き換えないといけないのが問題として微妙では？

---
+++ code/easy_26_remove_duplicates_from_sorted_array.go
@@ -0,0 +1,28 @@
+package code
+
+/*
+ * @lc app=leetcode id=26 lang=golang
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	// https://leetcode.com/problems/remove-duplicates-from-sorted-array/solutions/1067772/go-solution/?orderBy=most_votes&languageTags=golang
+	ln := len(nums)
+	if ln <= 1 {
+		return ln
+	}
+
+	j := 0 // points to  the index of last filled position
+	for i := 1; i < ln; i++ {
+		if nums[j] != nums[i] {
+			j++
+			nums[j] = nums[i]
+		}
+	}
+
+	return j + 1
+}
+
+// @lc code=end
