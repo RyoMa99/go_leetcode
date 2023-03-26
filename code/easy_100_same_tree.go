a31d7d89da738822e53f7b5894489c61518bac1b
rym49 <hatanakaryoma.130@gmail.com>
2023-03-26 17:16:58 +0900

    100-改良版
    true / falseの固定値ではなく判定地そのものを返せばよい

--- code/easy_100_same_tree.go
+++ code/easy_100_same_tree.go
@@ -17,22 +17,11 @@ package code
  */
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	if p == nil && q == nil {
-		return true
+	if p != nil && q != nil {
+		return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	} else {
+		return p == q
 	}
-
-	if p == nil || q == nil {
-		return false
-	}
-
-	if p.Val != q.Val {
-		return false
-	}
-
-	if isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right) {
-		return true
-	}
-	return false
 }
 
 // @lc code=end
